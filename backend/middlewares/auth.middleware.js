94e6d75c4c71d42da2dea6d2fba4f98f0383449c
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2024-02-15 23:00:32 +0530

    token

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const auth = (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.headers.authorization.split(" ")[1];
   if (token) {
     jwt.verify(token, "shlok", (err, decoded) => {
       if (decoded) {
